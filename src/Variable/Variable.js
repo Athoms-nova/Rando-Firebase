3c1dffd70c885bf64122f4fa4f8f6e496cd4ffcd
RAHERIMANANTSOA <65032937+Athoms-nova@users.noreply.github.com>
2023-10-15 21:25:57 +0400

    Add files via upload

---
+++ src/Variable/Variable.js
@@ -0,0 +1,52 @@
+const formatInformation = {
+  _id: "63ae8baa4025b9dd0bcbf106",
+  nom: "Le Trou de Fer par le Gîte de Bélouve",
+  ville: "Salazie",
+  longitude: "55.538418",
+  latitude: "-21.062556",
+  niveau: "Moyen",
+  duree: 300,
+  distance: 9480,
+  urlImageCard:
+    "https://images.unsplash.com/photo-1618773928121-c32242e63f39?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80",
+  information:
+    "Le Trou de Fer est un lieu emblématique de l’île de la Réunion. D'innombrables cascades se jettent dans cette immense et impressionnante cavité. Situé à l'Est de la Réunion, c'est un des lieux les plus arrosé de l’île ! Il est accessible depuis Bras-Panon et depuis la forêt de Bélouve. Depuis cette forêt dense, trois accès sont possibles : par le sentier du Trou de Fer, l'allée cavalière ou le sentier de l’École Normale.",
+  type: "Randonnée",
+  categorie: ["Foret", "Montagne"],
+  listeImages : [],
+  listeVideo : []
+};
+
+const listeCategorie = ["Montagne", "Plage", "Litoral", "Foret", "Bassin"];
+
+const listeType = ["All", "Randonnée", "Lieu"];
+
+const listeCommune = [
+  "All",
+  "Saint-Denis",
+  "Sainte-Marie",
+  "Sainte-Suzanne",
+  "Saint-Andre",
+  "Bras-Panon",
+  "Saint-Benoit",
+  "Sainte-Rose",
+  "Saint-Philippe",
+  "Saint-Joseph",
+  "Petite-ile",
+  "Saint-Pierre",
+  "Saint-Louis",
+  "Etang-Sale",
+  "Les Avirons",
+  "Saint-Leu",
+  "Les Trois Bassins",
+  "Saint-Paul",
+  "Le Port",
+  "La Possession",
+  "Salazie",
+  "Plaine des Palmistes",
+  "Le Tampon",
+  "Entre-Deux",
+  "Cilaos",
+];
+
+export { listeCategorie, listeCommune, listeType };
